4f8d54627894ab53b717a2d551fb8a7ee4ed933d
rivai5698 <ththang1998bg@gmail.com>
2022-10-26 06:27:57 +0700

    flutter hw14

--- lib/pages/address_detail_page.dart
+++ lib/pages/address_detail_page.dart
@@ -67,7 +67,7 @@ class _AddressDetailPageState extends State<AddressDetailPage> {
                     if(option){
                       setState(() {
                         filteredCity = widget.cities
-                            .where((u) => (u.name!
+                            .where((c) => (getNameArea(c.name!)
                             .toLowerCase()
                             .contains(string.toLowerCase())))
                             .toList();
@@ -75,7 +75,7 @@ class _AddressDetailPageState extends State<AddressDetailPage> {
                     }else{
                       setState(() {
                         filteredDistrict = districtList
-                            .where((u) => (u.name!
+                            .where((d) => (getNameArea(d.name!)
                             .toLowerCase()
                             .contains(string.toLowerCase())))
                             .toList();
@@ -174,6 +174,21 @@ class _AddressDetailPageState extends State<AddressDetailPage> {
       list.addAll(value);
     });
     return list;
+  }
+
+  getNameArea(String str){
+    str = str.toLowerCase();
+    str = str.replaceAll("à","a").replaceAll("á","a").replaceAll("ạ","a").replaceAll("ả","a").replaceAll("ã","a").replaceAll("â","a").replaceAll("ầ","a").replaceAll("ấ","a").replaceAll("ậ","a").replaceAll("ẩ","a").replaceAll("ẫ","a").replaceAll("ă","a").replaceAll("ằ","a").replaceAll("ắ","a").replaceAll("ặ","a").replaceAll("ẳ","a").replaceAll("ẵ","a");
+    str = str.replaceAll("è","e").replaceAll("é","e").replaceAll("ẹ","e").replaceAll("ẻ","e").replaceAll("ẽ","e").replaceAll("ê","e").replaceAll("ề","e").replaceAll("ế","e").replaceAll("ệ","e").replaceAll("ể","e").replaceAll("ễ","e");
+    str = str.replaceAll("ì","i").replaceAll("í","i").replaceAll("ị","i").replaceAll("ỉ","i").replaceAll("ĩ","i");
+    str = str.replaceAll("ò","o").replaceAll("ó","o").replaceAll("ọ","o").replaceAll("ỏ","o").replaceAll("õ","o").replaceAll("ô","o").replaceAll("ồ","o").replaceAll("ố","o").replaceAll("ộ","o").replaceAll("ổ","o").replaceAll("ỗ","o").replaceAll("ơ","o").replaceAll("ờ","o").replaceAll("ớ","o").replaceAll("ợ","o").replaceAll("ở","o").replaceAll("ỡ","o");
+    str = str.replaceAll("ù","u").replaceAll("ú","u").replaceAll("ụ","u").replaceAll("ủ","u").replaceAll("ũ","u").replaceAll("ư","u").replaceAll("ừ","u").replaceAll("ứ","u").replaceAll("ự","u").replaceAll("ử","u").replaceAll("ữ","u");
+    str = str.replaceAll("ỳ","y").replaceAll("ý","y").replaceAll("ỵ","y").replaceAll("ỷ","y").replaceAll("ỹ","y");
+    str = str.replaceAll("đ", "d");
+    str = str.replaceAll("\u0300","").replaceAll("\u0301","").replaceAll("\u0303","").replaceAll("\u0309","").replaceAll("\u0323","");
+    str = str.replaceAll("\u02C6","").replaceAll("\u0306","").replaceAll("\u031B/", "");
+    str = str.replaceAll(RegExp('[^A-Za-z0-9_ ]'), '');
+    return str;
   }
 
 }
